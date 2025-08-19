1b1f2dc864d6799ab7189a8630467c9174be3bee
PathaniaOO <77674592+PathaniaOO@users.noreply.github.com>
2025-08-19 16:15:29 +0530

    your message here

--- backend/gameapiapp/settings.py
+++ backend/gameapiapp/settings.py
@@ -86,13 +86,15 @@ WSGI_APPLICATION = 'gameapiapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PWD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
+
+
 
 
 # Password validation
