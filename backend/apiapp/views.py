1a49297e3bcfdb037653ea1817355aabac3a2c90
PathaniaOO <77674592+PathaniaOO@users.noreply.github.com>
2025-08-20 06:32:41 +0530

    procfile and added runtime

--- backend/apiapp/views.py
+++ backend/apiapp/views.py
@@ -54,7 +54,11 @@ class LeaderboardView(generics.ListAPIView):
 class GameListCreateView(generics.ListCreateAPIView):
     queryset = Game.objects.all()
     serializer_class = serializers.GameSerializer
-    permission_classes=[IsAuthenticatedOrReadOnly]
+    
+    def get_permissions(self):
+        if self.request.method == "GET":  # allow public GET
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()] 
 
 
     
